edece059c8fb1772859d6f0eb5ee8bcf41eb7e0d
Mohammad-Nobaveh-code <mohammadnobavehinformatiotech@gmail.com>
2022-08-10 11:00:49 +0430

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
+    path('', include('author.urls')),
+    path('', include('book.urls')),
+    path('', include('debt.urls')),
+    path('', include('extra.urls')),
+    path('', include('loan.urls')),
+]
